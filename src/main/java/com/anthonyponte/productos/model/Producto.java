8bcd00607945e12ae2ce3f4a7074a72456d3b74d
Anthony Ponte <anthonyponte@duck.com>
2025-03-08 22:41:20 -0500

    Implementando readAll y readById

---
+++ src/main/java/com/anthonyponte/productos/model/Producto.java
@@ -0,0 +1,65 @@
+package com.anthonyponte.productos.model;
+
+import java.io.Serializable;
+
+public class Producto implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+
+    public Producto() {
+    }
+
+    public Producto(int id, String nombre, String descripcion, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public Producto(String nombre, String descripcion, double precio) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", precio=" + precio
+                + "]";
+    }
+}
